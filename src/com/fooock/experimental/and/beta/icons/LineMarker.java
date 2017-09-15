b4f6ffa4baa141aeb4703161196b75ff189dcb72
newhouse <nhitbh@gmail.com>
2017-09-15 18:01:09 +0200

    Initial commit

---
+++ src/com/fooock/experimental/and/beta/icons/LineMarker.java
@@ -0,0 +1,28 @@
+package com.fooock.experimental.and.beta.icons;
+
+import com.intellij.codeInsight.daemon.LineMarkerInfo;
+import com.intellij.codeInsight.daemon.LineMarkerProvider;
+import com.intellij.psi.PsiElement;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ *
+ */
+public final class LineMarker implements LineMarkerProvider {
+
+    @Nullable
+    @Override
+    public LineMarkerInfo getLineMarkerInfo(@NotNull PsiElement psiElement) {
+        return null;
+    }
+
+    @Override
+    public void collectSlowLineMarkers(@NotNull List<PsiElement> list,
+                                       @NotNull Collection<LineMarkerInfo> collection) {
+
+    }
+}
